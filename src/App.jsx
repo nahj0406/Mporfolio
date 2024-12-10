638370f1c5b8b93dc8def6b6636fa0e1a201dc12
nahj0406 <nahj0406@naver.com>
2024-12-10 23:33:30 +0900

    lenis.js npm으로 설치하고 App.jsx에 적용 완료

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,34 @@ import Intro from './pages/Intro.jsx'
 import Greeting from './pages/greeting.jsx'
 import Portfolio from './pages/portfolio.jsx'
 import NightSky from './js/Nightsky.jsx'
+import Lenis from '@studio-freight/lenis';
 
 function App() {
 
 	// redux 변수 - 네비탭용
 	let naviTitle = useSelector((state)=> state.naviTitle)
 
-	// useEffect(() => {
-	// 	액티브업셋('active');
-	// }, []);
+	useEffect(() => {
+		// Lenis 인스턴스 생성
+		const lenis = new Lenis({
+			smoothWheel: true,
+      	smoothTouch: true,
+			wheelMultiplier: 0.5, // 휠 스크롤 속도 감소, 기본값 1
+      	touchMultiplier: 2, // 터치 스크롤 속도 증가 기본값 2
+		 });
+
+		// 애니메이션 루프 설정
+		function raf(time) {
+		  lenis.raf(time);
+		  requestAnimationFrame(raf);
+		}
+		requestAnimationFrame(raf);
+  
+		// 클린업: Lenis 인스턴스 제거
+		return () => {
+		  lenis.destroy();
+		};
+	}, []);
 
 	return (
 		<div className='App'>
